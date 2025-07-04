15e91b42c9ad99cefb2104bd8308614f8724056d
Berat ARPA <beratarpa@hotmail.com>
2025-07-04 05:43:18 +0300

    Service registerler yapıldı.

--- src/Services/IdentityService/IdentityService.API/Program.cs
+++ src/Services/IdentityService/IdentityService.API/Program.cs
@@ -1,22 +1,32 @@
+using IdentityService.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
-
-
-app.MapGet("/", () => "Identity Service Running!");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Identity Service API V1");
+        c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+    });
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
